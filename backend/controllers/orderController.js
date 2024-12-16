2989410fa24e309c2e99fab2380f1af6de1c848f
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 14:15:32 -0300

    Create the getAllOrders function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,15 @@
 const Order = require('../models/Order');
 
 
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        if (!orders){
+            return res.status(404).json({ message: "No orders found" });
+        }
+        res.render('ordersList', { orders });
+    } catch (error) {
+        console.error("Error fetching orders:", error.message);
+        res.status(500).json({ error: 'Error while fetching orders', message: error.message });
+    }
+}
