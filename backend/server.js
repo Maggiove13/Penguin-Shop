53857a65ecae2074f8289da58810957583c01bc7
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 13:33:10 -0300

    Config pug for the templates engine

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const createAdmin = require("./controllers/Admin");
+const router = require("./routes/routes");
+const path = require('path');
+const databaseConnection = require("./config/config");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
 dotenv.config();
 
 const app = express();
+
+// Config pug for engine 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
