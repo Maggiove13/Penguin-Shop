fcf0e8cb43d8dd0ff32490542b48d677b326ec68
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 13:48:28 -0300

    Create login route POST

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,3 +4,20 @@ const { loginAdmin, getDashboard, renderLoginPage, renderIndexPage, logout } = r
 const { authorization, adminMiddleware } = require('../middleware/auth');
 const { getProducts, createProduct, renderAddProduct, deleteProduct, updateProduct, renderEditProduct} = require("../controllers/productController");
 const { getAllOrders, updateStatus } = require('../controllers/orderController');
+
+router.post('/login', loginAdmin);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
