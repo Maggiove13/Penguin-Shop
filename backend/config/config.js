032038618df443e5dfb72b1346b19c86deb0df08
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 14:06:25 -0300

    add some config to the databaseConnection function

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,9 +5,13 @@ dotenv.config();
 
 const DB_URI = process.env.MONGO_URI;
 
-async function databaseConnection(){
+async function databaseConnection() {
     try {
-        await mongoose.connect(DB_URI);
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
     } catch (error) {
         console.error("Error connecting to the MongoDb. Error:", error);
     }
