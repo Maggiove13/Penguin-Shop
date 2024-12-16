e1218caa0f6fe37d83ed85fce0aa5cd9441472ef
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 14:16:03 -0300

    Create the updateStatus function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,3 +13,23 @@ exports.getAllOrders = async (req, res) => {
         res.status(500).json({ error: 'Error while fetching orders', message: error.message });
     }
 }
+
+
+exports.updateStatus = async (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    try {
+        const order = await Order.findByIdAndUpdate(
+            id,
+            { status },
+            { new: true }
+        );
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+        res.redirect('/orders'); // Redirige a la lista de órdenes después de la actualización
+    } catch (error) {
+        console.error("Error updating order status:", error.message);
+        res.status(500).json({ error: 'Error while updating order status', message: error.message });
+    }
+};
