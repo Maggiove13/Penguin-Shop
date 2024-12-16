455ce235e6348b96c3dd22a1f852ac5d40f654e4
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-16 13:47:28 -0300

    Create the routes.js file

---
+++ backend/routes/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { loginAdmin, getDashboard, renderLoginPage, renderIndexPage, logout } = require('../controllers/adminController');
+const { authorization, adminMiddleware } = require('../middleware/auth');
+const { getProducts, createProduct, renderAddProduct, deleteProduct, updateProduct, renderEditProduct} = require("../controllers/productController");
+const { getAllOrders, updateStatus } = require('../controllers/orderController');
